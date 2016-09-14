77198a887f4a476e2769d1a6ce7967b3def24a7a
Robespra <rpages07@gmail.com>
2016-09-14 21:24:46 +0200

    removed ugly menu

--- wp-content/themes/gridly/gridly/single.php
+++ wp-content/themes/gridly/gridly/single.php
@@ -1,48 +1,24 @@
 <?php get_header(); ?>
-
 <body class="cbp-spmenu-push">
-
-
-   <div class="padding10"></div>
-
-
-
-
+<div class="padding10"></div>
 <div class="la-anim-10"></div>
 <section>
 <header>
-       	        
-
- 	 	 <div id="nav">
+<div id="top-menu">
+<div id="nav">
 <h1 class="hidden-h1">Biscay Plain, créativité Art et design</h1>
 <a href="http://www.biscaypla.in"><img class="logo" src="<?php echo get_template_directory_uri(); ?>/images/headerimage_1.svg" alt="Biscay Plain, création artistique et psychologie de l'Art"/></a>  
 <!-- body has the class "cbp-spmenu-push" -->
 <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="cbp-spmenu-s2">
-  
-<div id="search"><?php get_search_form(); ?> 
-</div>
-    <a class="first white" href="http://www.biscaypla.in">Accueil</a>
-    <a class="ClickMe white">Langues  ►</a>
-    <div class="box-langue"><a>Spanish</a><a>English</a></div>	
-    <a class="white" href="http://www.biscaypla.in/Poesis_pdf_ebook.pdf">Telecharger eBook (pdf)</a>
-    <a class="ClickMeApp white">Télécharger App  ►</a>
-    
-<a class="ClickMeShare white">Partager  ►</a>
-    <div class="share"><a>Facebook</a><a>Twitter</a><a>Google +</a><a>Pinterest</a></div>	
-    <a class="white" href="http://www.biscaypla.in/a-propos/">A propos</a>
-    <a class="white" href="#">Guide de style</a>
-    <a class="white" href="#">Partenariat</a>
-    <a class="white" href="http://www.biscaypla.in/contact-2/">Contact</a>
-
+<div id="search"><?php get_search_form(); ?> </div>
 </nav>
 
 <section class="buttonset">
 <!-- Class "cbp-spmenu-open" gets applied to menu and "cbp-spmenu-push-toleft" or "cbp-spmenu-push-toright" to the body -->
 <div id="showRight" class="closed"><img src="<?php echo get_template_directory_uri(); ?>/images/menu.svg"/></div></section>
 </div>
-	  
-
-<!-- // header -->      
+<!-- // header -->   
+</div>   
 </header>   
 <div id="wrap" class="article">
 <article>
@@ -50,30 +26,6 @@
 <?php the_post_thumbnail( '' );  ?>
 </span>
 <div id="wrap-article">
-
-<script src="<?php echo get_template_directory_uri(); ?>/js/classie.js"></script>
-
-		<script>
-			var 	menuRight = document.getElementById( 'cbp-spmenu-s2' ),
-				showRightPush = document.getElementById( 'showRight' ),
-				body = document.body;
-		
-			showRight.onclick = function() {
-				classie.toggle( this, 'active' );
-				classie.toggle( menuRight, 'cbp-spmenu-open' );
-			};
-$('#showRight').click(function() {
-        if($('#showRight').hasClass('closed')) {
-            $(this).removeClass('closed').addClass('open').html('<img height="30px" style="position:fixed" src="<?php echo get_template_directory_uri();?>/images/deletebttn.png"/>');
-            $('#fade').fadeIn(); }
-        else if($('#showRight').hasClass('open')) {
-            $(this).removeClass('open').addClass('closed').html('<img src="<?php echo get_template_directory_uri(); ?>/images/menu.svg"/>');
-            $('#fade').fadeOut(); }
-    });
-		</script>
-
-
-  
  <div class="entry-content">
   <h2 class="header"><?php the_title(); ?></h2>
 <div class="clear padding60"></div>
@@ -84,14 +36,10 @@ $('#showRight').click(function() {
 
 	<p class="catekoi">
  	<a href="http://www.biscaypla.in">POESIS</a></p>		       			
-
 <div class="intro-excerp"><?php the_excerpt(); ?></div>
   <p class="tags"><?php the_time(get_option('date_format')); ?> 
 </br><?php the_tags(); ?> </p>
                            
-   
-
-
 <div class="padding20"></div>
  
            		 <?php the_content(); 
@@ -100,7 +48,6 @@ wpb_set_post_views(get_the_ID());
 ?> 
  
 <ul id="share">
-
 <li class="whatsapp"><div class="whatsapp-btn"><?php echo do_shortcode('[whatsapp]'); ?></div></li>
 <li class="facebook">
 <div id="fb-root"></div>
@@ -113,8 +60,6 @@ fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 <div class="fb-like" data-href="<?php the_permalink(); ?>" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
 </li>
-
-
 <li class="twitter">
 <a href="https://twitter.com/share" class="twitter-share-button" data-via="gamesnepal" data-related="gamesnepal" data-count="none">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
@@ -126,14 +71,7 @@ fjs.parentNode.insertBefore(js, fjs);
 </ul>
   <p class="tags" style="text-align:left;"><?php the_tags(); ?></p>
   <p class="tags" style="text-align:left;"><?php the_category(', '); ?></p>
-                 
-
-
-
-                 </div> 
-
-               
-
+</div> 
 </article>
 
 <section>
@@ -209,64 +147,8 @@ fjs.parentNode.insertBefore(js, fjs);
  <div class="padding10"></div>
                 <div class="clear"></div>
 
- 
 
 </aside>
-
-
-
-
-
-
-<script type="text/javascript" defer="">
-$('.box-langue').hide();
-$('.ClickMe').each(function() {
-    $(this).show(0).on('click', function(e) {
-       e.preventDefault();
-       $(this).next('.box-langue').slideToggle('fast', function() {
-            $(this).prev().html($(this).is(':visible') ? 'Langues &#9660;' : 'Langues &#9658');
-        });
-    });
-   if ($(this).data('box-show')) {
-        $(this).trigger('click');
-    }
-		
-});
-       </script> 
-<script type="text/javascript" defer="">
-$('.app').hide();
-$('.ClickMeApp').each(function() {
-    $(this).show(0).on('click', function(e) {
-       e.preventDefault();
-       $(this).next('.app').slideToggle('fast', function() {
-            $(this).prev().html($(this).is(':visible') ? 'Télécharger App  &#9660;' : 'Télécharger App  &#9658');
-        });
-    });
-   if ($(this).data('box-show')) {
-        $(this).trigger('click');
-    }
-		
-});
-       </script> 
-
-<script type="text/javascript" defer="">
-$('.share').hide();
-$('.ClickMeShare').each(function() {
-    $(this).show(0).on('click', function(e) {
-       e.preventDefault();
-       $(this).next('.share').slideToggle('fast', function() {
-            $(this).prev().html($(this).is(':visible') ? 'Partager  &#9660;' : 'Partager  &#9658');
-        });
-    });
-   if ($(this).data('box-show')) {
-        $(this).trigger('click');
-    }
-		
-});
-       </script> 
-
-
-
 
 
 </section>
@@ -326,3 +208,22 @@ $(window).scroll(function(){
 <?php get_footer(); ?>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 </footer>
+<script src="<?php echo get_template_directory_uri(); ?>/js/classie.js"></script>
+    <script>
+      var   menuRight = document.getElementById( 'cbp-spmenu-s2' ),
+        showRightPush = document.getElementById( 'showRight' ),
+        body = document.body;
+    
+      showRight.onclick = function() {
+        classie.toggle( this, 'active' );
+        classie.toggle( menuRight, 'cbp-spmenu-open' );
+      };
+$('#showRight').click(function() {
+        if($('#showRight').hasClass('closed')) {
+            $(this).removeClass('closed').addClass('open').html('<img height="30px" style="position:fixed" src="<?php echo get_template_directory_uri();?>/images/deletebttn.png"/>');
+            $('#fade').fadeIn(); }
+        else if($('#showRight').hasClass('open')) {
+            $(this).removeClass('open').addClass('closed').html('<img src="<?php echo get_template_directory_uri(); ?>/images/menu.svg"/>');
+            $('#fade').fadeOut(); }
+    });
+    </script> 
